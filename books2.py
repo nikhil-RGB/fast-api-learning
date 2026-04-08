32fb33f57a40fbb320fb8ea3e78ff8059e97771a
Nikhil Narayanan <javakingxi@gmail.com>
2026-04-08 17:40:43 +0530

    Add model and input validation

--- books2.py
+++ books2.py
@@ -1,4 +1,7 @@
+from typing import Optional
+
 from fastapi import FastAPI, Body
+from pydantic import BaseModel, Field
 app=FastAPI()
 # An object of class Book will be used in this updated model of the API.
 class Book:
@@ -15,6 +18,26 @@ class Book:
         self.description = description
         self.rating = rating
 
+class BookRequest(BaseModel):
+    id:Optional[int]= Field(description="ID is not needed on create",default=None) 
+    title:str=Field(min_length=3)
+    author:str=Field(min_length=1)
+    description:str=Field(min_length=1,max_length=100)
+
+    rating:int=Field(gt=0, lt=6)
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {                
+                "title": "A new book",
+                "author": "codingwithroby",
+                "description": "A new description of a book",
+                "rating": 5,
+                'published_date': 2029
+            }
+        }
+    }
+
 BOOKS = [
     Book(1, 'Computer Science Pro', 'codingwithroby', 'A very nice book!', 5),
     Book(2, 'Be Fast with FastAPI', 'codingwithroby', 'A great book!', 5),
@@ -27,3 +50,16 @@ BOOKS = [
 @app.get("/books")
 async def read_all_books():
     return BOOKS
+
+@app.post("/books/")
+async def create_book(new_book_req: BookRequest):
+    new_book=Book(**new_book_req.dict())
+    BOOKS.append(find_book_id(new_book))
+    return f"{new_book_req.title} has been added"
+
+def find_book_id(book:Book):
+    if len(BOOKS)>0:
+        book.id= BOOKS[-1].id + 1
+    else:
+        book.id=1
+    return book
