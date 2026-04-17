17a68108aca16da124c4875a4a021fed736894bf
Nikhil Narayanan <javakingxi@gmail.com>
2026-04-17 18:00:04 +0530

    add user route and endpoints

---
+++ todoapp/routers/users.py
@@ -0,0 +1,53 @@
+from typing import Annotated
+from pydantic import BaseModel, Field
+from starlette import status
+from fastapi import APIRouter, Depends, HTTPException, Path
+import models
+from database import SessionLocal
+from sqlalchemy.orm import Session
+from .auth import get_current_user
+from passlib.context import CryptContext
+
+router= APIRouter(
+    prefix="/user",
+    tags=['user']
+)
+
+
+
+def get_db():
+    db= SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency=Annotated[Session,Depends(get_db)]
+user_dependency=Annotated[dict,Depends(get_current_user)]
+bcrypt_context=CryptContext(schemes=['bcrypt'],deprecated='auto')
+
+class UserVerification(BaseModel):
+    password:str
+    new_password:str=Field(min_length=6)
+
+
+# Get all user information associated with current user as per user_dependency.
+@router.get("/",status_code=status.HTTP_200_OK)
+async def get_user(user:user_dependency,db:db_dependency):
+    if user is None:
+        raise HTTPException(status_code=401,detail="authentication failed")
+    return db.query(models.Users).filter(models.Users.id==user.get('id')).first()
+
+@router.put("/password",status_code=status.HTTP_200_OK)
+async def change_user_password(user:user_dependency,db:db_dependency,user_verification:UserVerification):
+    if user is None:
+        raise HTTPException(status_code=401,detail="authentication failed")
+    user_model=db.query(models.Users).filter(models.Users.id==user.get('id')).first()
+
+    if not bcrypt_context.verify(user_verification.password,user_model.hashed_password):
+        raise HTTPException(status_code=401,detail="Password mismatch")
+    
+    user_model.hashed_password=bcrypt_context.hash(user_verification.new_password)
+    db.add(user_model)
+    db.commit()
+    
