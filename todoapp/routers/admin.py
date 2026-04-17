2e85eaf66e25469e671fd3633ee295e7c8c9238a
Nikhil Narayanan <javakingxi@gmail.com>
2026-04-17 22:37:02 +0530

    Add extra endpoints to admin route(exp)

--- todoapp/routers/admin.py
+++ todoapp/routers/admin.py
@@ -28,3 +28,33 @@ async def read_all(user: user_dependency, db: db_dependency):
         raise HTTPException(status_code=401, detail='Authentication failed')
         
     return db.query(models.Todos).all()
+# created by me, delete a certain todo
+@router.delete("/todo/delete/{todo_id}",status_code=status.HTTP_204_NO_CONTENT)
+async def delete_todo(user:user_dependency,db:db_dependency,todo_id:int=Path(gt=0)):
+    if user is None or user.get('user_role')!='admin':
+        raise HTTPException(status_code=401, detail='Authentication failed')
+    todo_model=db.query(models.Todos).filter(models.Todos.id==todo_id).first()
+    if todo_model is None:
+        raise HTTPException(status_code=404, detail="Todo not found!")
+    db.delete(todo_model)
+    db.commit()
+# created by me, delete a certain user by their id
+@router.delete("/todo/delete_user/{owner_id}",status_code=status.HTTP_204_NO_CONTENT)
+async def delete_user(user:user_dependency,db:db_dependency,owner_id:int=Path(gt=0)):
+    if user is None or user.get('user_role')!='admin':
+        raise HTTPException(status_code=401, detail='Authentication failed')
+    user_model=db.query(models.Users).filter(models.Users.id==owner_id).first()
+    if user_model is None:
+        raise HTTPException(status_code=404, detail="User Not Found")
+    db.query(models.Todos).filter(models.Todos.owner_id == owner_id).delete()
+    db.delete(user_model)
+    db.commit()
+# created by me, get all users registered in the database
+@router.get("/todo/show_users",status_code=status.HTTP_200_OK)
+async def show_users(user:user_dependency,db:db_dependency,owner_id:int=Path(gt=0)):
+    if user is None or user.get('user_role')!='admin':
+        raise HTTPException(status_code=401, detail='Authentication failed')
+    user_models=db.query(models.Users).all()
+    return user_models
+
+
