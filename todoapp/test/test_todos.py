a37033569f1edc3da650290b997fcd5176fdc846
Nikhil Narayanan <javakingxi@gmail.com>
2026-05-19 14:18:54 +0530

    Add admin and todos tests

--- todoapp/test/test_todos.py
+++ todoapp/test/test_todos.py
@@ -1,31 +1,95 @@
-from sqlalchemy import create_engine
-from sqlalchemy import StaticPool
-from ..database import Base
-from ..main import app
+
 from ..routers.todos import get_db,get_current_user 
+from fastapi import status
+from ..models import Todos
+from .utils import *
 
-SQL_ALCHEMY_DATABASE_URL="sqlite:///./testdb.db"
-engine= create_engine(SQL_ALCHEMY_DATABASE_URL,
-                      connect_args={"check_same_thread":False},
-                      poolclass=StaticPool,
-                     )
 
-TestingSessionLocal=sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base.metadata.create_all(bind=engine)
-
-#Override dependency injection here-->
-def override_get_db():
-    db= TestingSessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-#Override function for getting current user-->
-def  override_get_current_user():
-    return {'username':'nikkupikku','id':1,'role':'admin'}
 
 app.dependency_overrides[get_db]= override_get_db
 app.dependency_overrides[get_current_user]= override_get_current_user
+
+
+# test for api endpoint "/"
+def test_read_all_authenticated(test_todo):
+    response=client.get("/")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == [{'complete':False, 'title':'Learn to code','description':"Need to learn everyday", 'priority':5,'id':1,'owner_id':1}]
+
+# test for what happens when a todo is not found
+def test_todo_not_found(test_todo):
+    response=client.get("/todo/999")
+    assert response.status_code== status.HTTP_404_NOT_FOUND
+    assert response.json()=={'detail':'Todo not found'}
+
+#test for creation of todo object
+def test_todo_creation(test_todo):
+    todo_req={
+        'title':'Test todo',
+        'description':'Test todo description',
+        'priority':2,
+        'complete':False,
+    }
+    response=client.post("/todo",json=todo_req)
+    assert response.status_code==status.HTTP_201_CREATED
+
+    db=TestingSessionLocal()
+    todo_model=db.query(Todos).filter(Todos.id==2).first()
+    # Assertions to check if the todo item actually exists in the test table
+    assert todo_model.title==todo_req.get('title')
+    assert todo_model.description==todo_req.get('description')
+    assert todo_model.priority==todo_req.get('priority')
+    assert todo_model.complete==todo_req.get('complete')
+
+#test for updation of an existing todo entry
+def test_todo_updation(test_todo):
+    todo_req={
+        'title':'Updated Test todo',
+        'description':'Updated Test todo description',
+        'priority':2,
+        'complete':True,
+    }
+    response=client.put("/todo/1",json=todo_req)
+    assert response.status_code==status.HTTP_204_NO_CONTENT
+    db=TestingSessionLocal()
+    todo_model=db.query(Todos).filter(Todos.id==1).first()
+    assert todo_model.title==todo_req.get('title')
+    assert todo_model.description==todo_req.get('description')
+    assert todo_model.priority==todo_req.get('priority')
+    assert todo_model.complete==todo_req.get('complete')
+    
+# Test for failed put operation
+def test_update_todo_not_found(test_todo):
+    todo_req={
+        'title':'Updated Test todo',
+        'description':'Updated Test todo description',
+        'priority':2,
+        'complete':True,
+    }
+    response=client.put("/todo/999",json=todo_req)
+    assert response.status_code==status.HTTP_404_NOT_FOUND
+    assert response.json()=={'detail':'Todo not found'}
+# Test for deletion operation
+def test_delete_todo(test_todo):
+    response=client.delete("/todo/1")
+    assert response.status_code==status.HTTP_204_NO_CONTENT
+    db=TestingSessionLocal()
+    todo_model=db.query(Todos).filter(Todos.id==1).first()
+    assert todo_model is None
+# Test for todo not found when deletion requested
+def test_delete_todo_not_found():
+    response = client.delete('/todos/todo/999')
+    assert response.status_code == 404
+    assert response.json() == {'detail': 'Not Found'}
+
+
+
+
+
+
+
+
+
+
 
 
---
+++ todoapp/test/utils.py
@@ -0,0 +1,49 @@
+﻿from fastapi.testclient import TestClient
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import StaticPool
+from ..database import Base
+from ..main import app
+from ..models import Todos
+import pytest
+SQL_ALCHEMY_DATABASE_URL="sqlite:///./testdb.db"
+engine= create_engine(SQL_ALCHEMY_DATABASE_URL,
+                      connect_args={"check_same_thread":False},
+                      poolclass=StaticPool,
+                     )
+
+TestingSessionLocal=sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base.metadata.create_all(bind=engine)
+
+#Override dependency injection here-->
+def override_get_db():
+    db= TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+#Override function for getting current user-->
+def  override_get_current_user():
+    return {'username':'nikkupikku','id':1,'user_role':'admin'}
+
+
+client=TestClient(app)
+
+#Fixture for creating a test-todo and then deleting it
+@pytest.fixture
+def test_todo():
+    todo= Todos(
+        title="Learn to code",
+        description="Need to learn everyday",
+        priority=5,
+        owner_id=1,
+        id=1,
+    )
+    db=TestingSessionLocal()
+    db.add(todo)
+    db.commit()
+    yield todo
+    with engine.connect() as connection:
+        connection.execute(text("DELETE FROM todos;"))
+        connection.commit()
